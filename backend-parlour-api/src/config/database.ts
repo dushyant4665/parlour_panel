7b8473d42a84af7e5be62bf5d70f5098cdd3ab1d
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2025-07-06 17:32:24 -0700

    Update database.ts

--- backend-parlour-api/src/config/database.ts
+++ backend-parlour-api/src/config/database.ts
@@ -1,16 +1,14 @@
 import mongoose from "mongoose"
-
 const connectDB = async () => {
   try {
     const mongoUri = process.env.MONGODB_URI;
-    console.log('MONGODB_URI from env:', mongoUri);
+     console.log('MONGODB_URI from env:', mongoUri);
     if (!mongoUri) throw new Error("MONGODB_URI not set in environment");
-    await mongoose.connect(mongoUri);
+     await mongoose.connect(mongoUri);
     console.log('✅ MongoDB Connected Successfully!');
   } catch (error) {
-    console.error('❌ MongoDB connection error:', error);
+     console.error('❌ MongoDB connection error:', error);
     process.exit(1)
   }
 }
-
 export default connectDB
